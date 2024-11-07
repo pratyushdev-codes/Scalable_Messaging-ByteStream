a2f23142f159ebbef6eb46e6e016378b1de2c7c2
Pratyush Birole <birole.pratyush@gmail.com>
2024-11-07 23:40:17 +0530

    Fixed Backend --> socket.ts initalization errors

--- apps/server/services/socket.ts
+++ apps/server/services/socket.ts
@@ -1,31 +1,29 @@
 import { Server } from "socket.io";
 
-
-
-class SocketService{
+class SocketService {
     private _io: Server;
 
+    constructor() {
+        console.log("Init Socket Server....");
 
-    constructor(){
-        console.log("Init Socket Server....")
-
-        this._io= new Server;
+        // Initialize the server properly
+        this._io = new Server();
     }
 
-
-
-    public initListeners(){
+    public initListeners() {
         const io = this.io;
-        io.on("connect" , (socket)=>{
+        io.on("connect", (socket) => {
             console.log("Client connected", socket.id);
-        })
-        
+
+            socket.on('event:message', async ({ message }: { message: string }) => {
+                console.log("New Message received", message);
+            });
+        });
     }
 
-    get io(){
+    get io() {
         return this._io;
     }
-
 }
 
-export default SocketService
+export default SocketService;
